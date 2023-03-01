363550c83682af2c8a0862c6f6f770a34439155f
buraksenyurt <burakselimsenyurt@gmail.com>
2023-03-01 16:37:55 +0300

    Added some endpoints.

--- RockShop.WebApi.Service/Program.cs
+++ RockShop.WebApi.Service/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OpenApi;
 using RockShop.Shared;
+using RockShop.WebApi.Service.Dtos.Response;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,7 +20,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/Ping", () => "Pong");
+app.MapGet("/ping", () => "Pong").ExcludeFromDescription();
 
 int pageSize = 5;
 app.MapGet("api/albums", (
@@ -33,6 +34,45 @@ app.MapGet("api/albums", (
     })
     .Produces<Album[]>(StatusCodes.Status200OK);
 
+app.MapGet("api/artists", (
+    [FromServices] ChinookDbContext db,
+    [FromQuery] int? page) =>
+        db.Artists.Skip(((page ?? 1) - 1) * pageSize).Take(pageSize)).WithName("GetArtists")
+    .WithOpenApi(operation =>
+    {
+        operation.Description = "Get artists with paging";
+        return operation;
+    })
+    .Produces<Artist[]>(StatusCodes.Status200OK);
+
+app.MapGet("api/tracks", (
+    [FromServices] ChinookDbContext db,
+    [FromQuery] int? page) =>
+        {
+            var result = from t in db.Tracks
+                         join a in db.Albums on t.AlbumId equals a.AlbumId
+                         join g in db.Genres on t.GenreId equals g.GenreId
+                         join m in db.MediaTypes on t.MediaTypeId equals m.MediaTypeId
+                         select new TrackDto
+                         {
+                             Name = t.Name,
+                             Composer = t.Composer,
+                             Duration = t.Milliseconds,
+                             UnitPrice = t.UnitPrice,
+                             Album = a.Title,
+                             Genre = g.Name,
+                             MediaType = m.Name
+                         };
+            return result.Skip(((page ?? 1) - 1) * pageSize).Take(pageSize);
+        })
+    .WithName("GetTracks")
+    .WithOpenApi(operation =>
+    {
+        operation.Description = "All tracks with paging";
+        return operation;
+    })
+    .Produces<TrackDto>(StatusCodes.Status200OK);
+
 
 app.MapGet("api/albums/{id:int}", async Task<Results<Ok<Album>, NotFound>> (
     [FromServices] ChinookDbContext db,
@@ -45,15 +85,24 @@ app.MapGet("api/albums/{id:int}", async Task<Results<Ok<Album>, NotFound>> (
         operation.Description = "Get album by id";
         return operation;
     })
-    .Produces<Album[]>(StatusCodes.Status200OK)
+    .Produces<Album>(StatusCodes.Status200OK)
     .Produces(StatusCodes.Status404NotFound);
+
 
 app.MapGet("api/customers/{country}", (
     [FromServices] ChinookDbContext db,
     [FromRoute] string country) =>
         db.Customers
             .Where(c => c.Country == country)
-            .Select(c => new { c.CustomerId, c.FirstName, c.LastName, c.City, c.Email })
+            .Select(c => new CustomerDto
+            {
+                CustomerId = c.CustomerId,
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                City = c.City,
+                Email = c.Email
+            }
+            )
             .OrderBy(c => c.LastName))
     .WithName("GetCustomersByCountry")
     .WithOpenApi(operation =>
@@ -61,26 +110,140 @@ app.MapGet("api/customers/{country}", (
         operation.Description = "Get customers by Country";
         return operation;
     })
-    .Produces<Customer>(StatusCodes.Status200OK);
+    .Produces<CustomerDto>(StatusCodes.Status200OK);
 
-app.MapGet("api/invoices/totalsales/top/five", (
-    [FromServices] ChinookDbContext db) =>
-        (from i in db.Invoices
-         group i by i.BillingCountry into g
-         select new
-         {
-             BillingCountry = g.Key,
-             TotalSales = g.Sum(x => x.Total)
-         }).OrderByDescending(x => x.TotalSales)
-         .Take(5)
+app.MapGet("api/invoices/totalsales/top/{count}", (
+    [FromServices] ChinookDbContext db,
+    [FromRoute] int count) =>
+        {
+            if (count <= 0 || count > 10)
+                return null;
+
+            return (from i in db.Invoices
+                    group i by i.BillingCountry into g
+                    select new TotalSalesByCountryDto
+                    {
+                        Country = g.Key,
+                        Total = g.Sum(x => x.Total)
+                    }).OrderByDescending(x => x.Total)
+            .Take(count);
+        }
     )
     .WithName("TotalSalesByCountry")
     .WithOpenApi(operation =>
     {
-        operation.Description = "Total Sales By Country(Top Five)";
+        operation.Description = "Total Sales By Country (with count)";
         return operation;
     })
-    .Produces<Invoice>(StatusCodes.Status200OK);
+    .Produces<TotalSalesByCountryDto>(StatusCodes.Status200OK);
+
+app.MapGet("api/albums/{artistName}", (
+    [FromServices] ChinookDbContext db,
+    [FromRoute] string artistName) =>
+    {
+        if (string.IsNullOrEmpty(artistName))
+            return null;
+
+        return (from a in db.Albums
+                join ar in db.Artists
+                on a.ArtistId equals ar.ArtistId
+                where !string.IsNullOrEmpty(ar.Name) && ar.Name.ToUpper().StartsWith(artistName.ToUpper())
+                select a).OrderBy(a => a.Title);
+    }
+    )
+    .WithName("GetAlbumsByArtistName")
+    .WithOpenApi(operation =>
+    {
+        operation.Description = "Get albums by artist name";
+        return operation;
+    })
+    .Produces<List<Album>>(StatusCodes.Status200OK);
+
+app.MapGet("api/artists/{id:int}", async Task<Results<Ok<Artist>, NotFound>> (
+    [FromServices] ChinookDbContext db,
+    [FromRoute] int? id) =>
+        await db.Artists.FindAsync(id) is Artist artist ? TypedResults.Ok(artist) : TypedResults.NotFound()
+    )
+    .WithName("GetArtistById")
+    .WithOpenApi(operation =>
+    {
+        operation.Description = "Get artist by id";
+        return operation;
+    })
+    .Produces<Artist>(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status404NotFound);
+
+app.MapDelete("api/albums/{id:int}", async (
+    [FromServices] ChinookDbContext db,
+    [FromRoute] int? id) =>
+    {
+        if (await db.Albums.FindAsync(id) is Album album)
+        {
+            db.Albums.Remove(album);
+            await db.SaveChangesAsync();
+            return Results.NoContent();
+
+        }
+        return Results.NotFound();
+    })
+    .WithName("DeleteAlbumById")
+    .WithOpenApi(operation =>
+    {
+        operation.Description = "Delete album by id";
+        return operation;
+    })
+    .Produces(StatusCodes.Status204NoContent)
+    .Produces(StatusCodes.Status404NotFound);
+
+app.MapPost("api/artists", async ([FromServices] ChinookDbContext db, [FromBody] PostArtistDto artist) =>
+{
+    var newArtist = new Artist
+    {
+        Name = artist.Name
+    };
+    var createdArtist = await db.Artists.AddAsync(newArtist);
+    await db.SaveChangesAsync();
+
+    await db.Albums.AddRangeAsync(artist.Albums.Select(a => new Album
+    {
+        Title = a.Title ?? "None",
+        ArtistId = createdArtist.Entity.ArtistId
+    }).ToArray());
+    await db.SaveChangesAsync();
+
+    return Results.Created($"api/artists/{createdArtist.Entity.ArtistId}", newArtist);
+})
+.WithName("CreateArtist")
+.WithOpenApi(operation =>
+    {
+        operation.Description = "Create a new Artist";
+        return operation;
+    })
+.Produces<PostArtistDto>(StatusCodes.Status201Created);
+
+app.MapPut("api/artists/{id:int}", async (
+    [FromRoute] int id,
+    [FromBody] PutArtistDto artistDto,
+    [FromServices] ChinookDbContext db) =>
+{
+    Artist? founded = await db.Artists.FindAsync(id);
+    if (founded is null)
+        return Results.NotFound();
+
+    founded.Name = artistDto.Name;
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+}
+)
+.WithName("UpdateArtistName")
+.WithOpenApi(operation =>
+    {
+        operation.Description = "Update artist name by id";
+        return operation;
+    })
+.Produces(StatusCodes.Status404NotFound)
+.Produces(StatusCodes.Status204NoContent);
 
 
 app.Run();
