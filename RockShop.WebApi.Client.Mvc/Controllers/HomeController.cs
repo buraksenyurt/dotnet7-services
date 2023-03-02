c4b457e1ea2461d390dfbdabc5e891b46cbcf4be
buraksenyurt <burakselimsenyurt@gmail.com>
2023-03-02 19:22:45 +0300

    Applied the CORS with Http Client Factory.

--- RockShop.WebApi.Client.Mvc/Controllers/HomeController.cs
+++ RockShop.WebApi.Client.Mvc/Controllers/HomeController.cs
@@ -1,16 +1,19 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using RockShop.WebApi.Client.Mvc.Models;
+using RockShop.Shared;
 
 namespace RockShop.WebApi.Client.Mvc.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IHttpClientFactory _clientFactory;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IHttpClientFactory clientFactory)
     {
         _logger = logger;
+        _clientFactory = clientFactory;
     }
 
     public IActionResult Index()
@@ -27,5 +30,16 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [Route("home/albums/{artistName?}")]
+    public async Task<IActionResult> Albums(string? artistName)
+    {
+        var client = _clientFactory.CreateClient(name: "RockShop.WebApi.Service");
+        var request = new HttpRequestMessage(method: HttpMethod.Get, requestUri: $"api/albums/{artistName}");
+        var response = await client.SendAsync(request);
+        IEnumerable<Album>? albums = await response.Content.ReadFromJsonAsync<IEnumerable<Album>>();
+        ViewData["apiAddress"] = client.BaseAddress;
+        return View(albums);
     }
 }
--- RockShop.WebApi.Client.Mvc/Program.cs
+++ RockShop.WebApi.Client.Mvc/Program.cs
@@ -1,15 +1,22 @@
+using System.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddHttpClient(name: "RockShop.WebApi.Service",
+configureClient: options =>
+{
+    options.BaseAddress = new("http://localhost:5221/");
+    options.DefaultRequestHeaders.Accept.Add(
+        new MediaTypeWithQualityHeaderValue("application/json", 1.0)
+    );
+});
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
