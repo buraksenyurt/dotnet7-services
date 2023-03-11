f0b3c2cb456f3292a66170aa1129eb85f0f099e2
buraksenyurt <burakselimsenyurt@gmail.com>
2023-03-11 21:30:15 +0300

    Completed sample OData search.

---
+++ RockShop.OData.Client.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RockShop.OData.Client.Mvc.Models;
+using RockShop.OData.Client.Mvc.Models.OData;
+
+namespace RockShop.OData.Client.Mvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IHttpClientFactory _clientFactory;
+
+    public HomeController(ILogger<HomeController> logger, IHttpClientFactory clientFactory)
+    {
+        _logger = logger;
+        _clientFactory = clientFactory;
+    }
+
+    public async Task<IActionResult> Index(string nameWith = "Abc")
+    {
+        try
+        {
+            var client = _clientFactory.CreateClient(name: "RockShop.OData");
+            var request = new HttpRequestMessage(
+                method: HttpMethod.Get
+                , requestUri: $"jukebox/v1/albums/?$filter=startswith(Title,'{nameWith}')&select=AlbumId,Title");
+            var response = await client.SendAsync(request);
+
+            ViewData["albumNameStartsWith"] = nameWith;
+            ViewData["albums"] = (await response.Content.ReadFromJsonAsync<Albums>())?.Value;
+
+        }
+        catch (Exception e)
+        {
+            _logger.LogError($"OData service client error. Exception detail {e.Message}");
+        }
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
