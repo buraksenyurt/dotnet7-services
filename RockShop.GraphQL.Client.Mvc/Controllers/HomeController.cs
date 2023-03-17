26f3876e37202a8d9238e8a6773851cdc03d7aca
buraksenyurt <burakselimsenyurt@gmail.com>
2023-03-17 22:49:42 +0300

    Added controller and view.

--- RockShop.GraphQL.Client.Mvc/Controllers/HomeController.cs
+++ RockShop.GraphQL.Client.Mvc/Controllers/HomeController.cs
@@ -1,21 +1,64 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using RockShop.GraphQL.Client.Mvc.Models;
+using System.Text;
 
 namespace RockShop.GraphQL.Client.Mvc.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IHttpClientFactory _clientFactory;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IHttpClientFactory clientFactory)
     {
         _logger = logger;
+        _clientFactory = clientFactory;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index(string pageNumber = "1")
     {
-        return View();
+        IndexViewModel model = new();
+        try
+        {
+            var client = _clientFactory.CreateClient(name: "RockShop.GraphQL");
+            HttpRequestMessage request = new(method: HttpMethod.Get, requestUri: "/");
+            var response = await client.SendAsync(request);
+            if (!response.IsSuccessStatusCode)
+            {
+                model.StatusCode = response.StatusCode;
+                model.Errors = new[] { new Error { Message = "Service is not availably right know. Try again later." } };
+                return View(model);
+            }
+
+            request = new(method: HttpMethod.Post, requestUri: "graphql");
+            // Used C#11 raw interpolated string literal syntax
+            request.Content = new StringContent(content: $$$"""
+            {
+                "query":"{tracks(page: {{{pageNumber}}}){album name composer}}"
+            }
+            """, encoding: Encoding.UTF8,
+            mediaType: "application/json");
+            response = await client.SendAsync(request);
+
+            model.StatusCode = response.StatusCode;
+            model.RawResponse = await response.Content.ReadAsStringAsync();
+
+            if (response.IsSuccessStatusCode)
+            {
+                model.Tracks = (await response.Content.ReadFromJsonAsync<ResponseTrack>())?.Data?.Tracks;
+            }
+            else
+            {
+                model.Errors = (await response.Content.ReadFromJsonAsync<ErrorResponse>())?.Errors;
+            }
+        }
+        catch (Exception exp)
+        {
+            _logger.LogError($"RockShop.GraphQL service exception: {exp.Message}");
+            model.Errors = new[] { new Error { Message = exp.Message } };
+        }
+        return View(model);
     }
 
     public IActionResult Privacy()
--- RockShop.GraphQL.Client.Mvc/Program.cs
+++ RockShop.GraphQL.Client.Mvc/Program.cs
@@ -1,15 +1,21 @@
+using System.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Registered the HttpClient for calling GraphQL service
+builder.Services.AddHttpClient(name: "RockShop.GraphQL", configureClient: options =>
+{
+    options.BaseAddress = new Uri("http://localhost:5034/");
+    options.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json", 1.0));
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
