810d0e388cc76250f09a84abb6f7f0d87713e345
buraksenyurt <burakselimsenyurt@gmail.com>
2023-03-18 00:49:55 +0300

    Added a simple graphql query to console.

--- RockShop.GraphQL.Client.Console/Program.cs
+++ RockShop.GraphQL.Client.Console/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StrawberryShake;
+using Microsoft.Extensions.DependencyInjection;
+using RockShop.GraphQL.Client.Console;
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddRockShopClient().ConfigureHttpClient(client=>client.BaseAddress= new Uri("http://localhost:5034/graphql"));
+IServiceProvider services = serviceCollection.BuildServiceProvider();
+IRockShopClient client = services.GetRequiredService<IRockShopClient>();
+
+var result = await client.GetTracks.ExecuteAsync();
+result.EnsureNoErrors();
+
+foreach(var trk in result.Data.Tracks){
+    Console.WriteLine($"{trk.Album},{trk.Name},{trk.Composer}");
+}
