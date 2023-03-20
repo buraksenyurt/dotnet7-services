9f3ed1fb01493b9a3503a34e6b6ea2e033da508e
buraksenyurt <burakselimsenyurt@gmail.com>
2023-03-21 00:31:24 +0300

    Redesigned controller to call grpc service.

--- RockShop.Grpc.Client.Mvc/Controllers/HomeController.cs
+++ RockShop.Grpc.Client.Mvc/Controllers/HomeController.cs
@@ -1,20 +1,34 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using RockShop.Grpc.Client.Mvc.Models;
+using Grpc.Net.Client;
+using Grpc.Net.ClientFactory;
 
 namespace RockShop.Grpc.Client.Mvc.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly JukeBox.JukeBoxClient _jukeBoxClient;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, GrpcClientFactory factory)
     {
         _logger = logger;
+        _jukeBoxClient = factory.CreateClient<JukeBox.JukeBoxClient>("JukeBox");
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index(int pn = 1)
     {
+        try
+        {
+            ArtistReply reply = await _jukeBoxClient.GetArtistsAsync(new Mvc.ArtistRequest { PageNumber = pn });
+            ViewData["artistList"] = reply.Data;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError("RockShop service is not responding.");
+            ViewData["exception"] = e.Message;
+        }
         return View();
     }
 
