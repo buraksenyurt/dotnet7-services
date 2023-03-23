d00008d4171c660464508293a85b6c110c41a959
buraksenyurt <burakselimsenyurt@gmail.com>
2023-03-23 22:05:33 +0300

    Equipped with Swagger ability.

--- RockShop.Grpc.Service.Json/Program.cs
+++ RockShop.Grpc.Service.Json/Program.cs
@@ -1,10 +1,19 @@
 using RockShop.Grpc.Service.Json.Services;
 using RockShop.Shared;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // JSON supoort for GRPC
 builder.Services.AddGrpc().AddJsonTranscoding();
+// Swagger support
+builder.Services.AddGrpcSwagger();
+builder.Services.AddSwaggerGen(options =>
+                               options.SwaggerDoc("v1", new OpenApiInfo
+                               {
+                                   Title = "Rock Shop gRPC Services",
+                                   Version = "v1"
+                               }));
 
 // EF Support
 builder.Services.AddChinookDbContext();
@@ -12,5 +21,8 @@ builder.Services.AddChinookDbContext();
 var app = builder.Build();
 
 app.MapGrpcService<JukeBoxService>();
+
+app.UseSwagger();
+app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "RockShop gRPC v1"));
 
 app.Run();
--- RockShop.Grpc.Service.Json/Services/JukeBoxService.cs
+++ RockShop.Grpc.Service.Json/Services/JukeBoxService.cs
@@ -3,17 +3,29 @@ using RockShop.Shared;
 
 namespace RockShop.Grpc.Service.Json.Services;
 
+/// <summary>
+/// JukeBox grpcService
+/// </summary>
 public class JukeBoxService
     : JukeBox.JukeBoxBase
 {
     private readonly ILogger<JukeBoxService> _logger;
     private readonly ChinookDbContext _dbContext;
+    /// <summary>
+    /// Constructor
+    /// </summary>
     public JukeBoxService(ILogger<JukeBoxService> logger, ChinookDbContext dbContext)
     {
         _logger = logger;
         _dbContext = dbContext;
     }
 
+    /// <summary>
+    /// Get albums with paging
+    /// </summary>
+    /// <param name="request">Includes page number</param>
+    /// <param name="context">Server call context</param>
+    /// <returns>Album names</returns>
     public override Task<AlbumReply> GetAlbums(AlbumRequest request, ServerCallContext context)
     {
         var albums = _dbContext
