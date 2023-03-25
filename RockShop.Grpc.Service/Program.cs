62068b155cef9274f2a84c933151727e9bf46645
buraksenyurt <burakselimsenyurt@gmail.com>
2023-03-25 17:49:34 +0300

    Added server reflection support for dev env.

--- RockShop.Grpc.Service/Program.cs
+++ RockShop.Grpc.Service/Program.cs
@@ -4,10 +4,22 @@ using RockShop.Shared;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
+
+// Server Reflection support
+builder.Services.AddGrpcReflection();
+
+// EF
 builder.Services.AddChinookDbContext();
 
 var app = builder.Build();
 
 app.MapGrpcService<JukeBoxService>();
+
+// Serve Reflection support
+IWebHostEnvironment env = app.Environment;
+if (env.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
 
 app.Run();
