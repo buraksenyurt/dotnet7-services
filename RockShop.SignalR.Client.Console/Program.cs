779cbb4fc4753a8f4e942fda17036fdbd5bcf191
buraksenyurt <burakselimsenyurt@gmail.com>
2023-03-31 14:48:06 +0300

    Completed hub communication from client.

--- RockShop.SignalR.Client.Console/Program.cs
+++ RockShop.SignalR.Client.Console/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using RockShop.Common.Models.SignalR;
+
+WriteLine("Wellcome community trace board.\n");
+Write("What is your name?");
+string? personName = ReadLine();
+
+if (!string.IsNullOrEmpty(personName))
+{
+    WriteLine("Please enter your interested topics for suggestion board");
+    string? topics = ReadLine();
+
+    HubConnection conn = new HubConnectionBuilder()
+        .WithUrl("https://localhost:7044/board",
+                    (opts) =>
+                    {
+                        opts.HttpMessageHandlerFactory = (message) =>
+                        {
+                            if (message is HttpClientHandler clientHandler)
+                                // to bypass SSL certificate(use for development only)
+                                clientHandler.ServerCertificateCustomValidationCallback +=
+                                    (sender, certificate, chain, sslPolicyErrors) => { return true; };
+                            return message;
+                        };
+                    })
+        .Build();
+
+    conn.On<Suggestion>("ReceiveSuggestion", message =>
+    {
+        WriteLine($"From {message.From} To {message.To} : {message.Content}");
+    });
+
+    await conn.StartAsync();
+    WriteLine("Board is online");
+
+    Person registration = new()
+    {
+        Name = personName,
+        Topics = topics
+    };
+
+    await conn.InvokeAsync("Register", registration);
+
+    WriteLine("Registration completed.\nBoard active.");
+    WriteLine("Press any key to exit");
+    ReadLine();
+
+}
+else
+{
+    WriteLine("No Name. Bye!");
+    return;
+}
