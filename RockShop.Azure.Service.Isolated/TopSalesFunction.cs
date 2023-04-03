374ff64ce29464b8db0e6731d4082a15b0068e4c
buraksenyurt <burakselimsenyurt@gmail.com>
2023-04-03 23:59:30 +0300

    Fixed some errors.

--- RockShop.Azure.Service.Isolated/TopSalesFunction.cs
+++ RockShop.Azure.Service.Isolated/TopSalesFunction.cs
@@ -18,29 +18,52 @@ namespace RockShop.Azure.Service.Isolated
         [Function("TopSalesFunction")]
         public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            _logger.LogInformation($"Request method is '{req.Method}'");
+            Region? region = null;
 
-            var body = await new StreamReader(req.Body).ReadToEndAsync();
-            _logger.LogInformation($"Incoming data {body}");
-            var input = JsonSerializer.Deserialize<Region>(body) ?? new Region { Name = "COMMON" };
+            if (req.Method == "POST")
+            {
+                try
+                {
+                    var body = await new StreamReader(req.Body).ReadToEndAsync();
+                    _logger.LogInformation($"Incoming data {body}");
+                    region = JsonSerializer.Deserialize<Region>(body);
+                }
+                catch (Exception excp)
+                {
+                    _logger.LogCritical($"{excp.Message}");
+                }
+            }
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "application/json");
 
-            var topSales = new List<Product>{
+            var products = new List<Product>{
                 new Product{Name="AMZN",Total=1200.50M},
                 new Product{Name="FCBK",Total=1090.93M},
                 new Product{Name="GOOG",Total=985.93M},
                 new Product{Name="TWTR",Total=250.00M},
                 new Product{Name="TORB",Total=4500.55M}
             };
-            var data = JsonSerializer.Serialize(topSales);
+
+            var report = new Report
+            {
+                Region = region ?? new Region { Name = "COMMON" },
+                Products = products
+            };
+
+            var data = JsonSerializer.Serialize(report);
             await response.WriteStringAsync(data);
 
             return await Task.FromResult(response);
         }
     }
 
+    public class Report
+    {
+        public Region Region { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+    }
     public class Product
     {
         public string Name { get; set; }
