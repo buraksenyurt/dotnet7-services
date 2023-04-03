00781b49c5e9cf1e8b9c0c274f5697ecd8b620cf
buraksenyurt <burakselimsenyurt@gmail.com>
2023-04-03 23:45:41 +0300

    Changed the azure function to more useful one.

---
+++ RockShop.Azure.Service.Isolated/TopSalesFunction.cs
@@ -0,0 +1,54 @@
+using System.Net;
+using System.Text.Json;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace RockShop.Azure.Service.Isolated
+{
+    public class TopSalesFunction
+    {
+        private readonly ILogger _logger;
+
+        public TopSalesFunction(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<TopSalesFunction>();
+        }
+
+        [Function("TopSalesFunction")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var body = await new StreamReader(req.Body).ReadToEndAsync();
+            _logger.LogInformation($"Incoming data {body}");
+            var input = JsonSerializer.Deserialize<Region>(body) ?? new Region { Name = "COMMON" };
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "application/json");
+
+            var topSales = new List<Product>{
+                new Product{Name="AMZN",Total=1200.50M},
+                new Product{Name="FCBK",Total=1090.93M},
+                new Product{Name="GOOG",Total=985.93M},
+                new Product{Name="TWTR",Total=250.00M},
+                new Product{Name="TORB",Total=4500.55M}
+            };
+            var data = JsonSerializer.Serialize(topSales);
+            await response.WriteStringAsync(data);
+
+            return await Task.FromResult(response);
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Total { get; set; }
+    }
+
+    public class Region
+    {
+        public string Name { get; set; }
+    }
+}
